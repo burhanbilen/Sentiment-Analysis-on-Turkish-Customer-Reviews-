910aa5fdd517b3d6f1f1cfe97d2900981e72043c
Burhan Bilen <34451356+burhanbilen@users.noreply.github.com>
2020-10-26 17:10:53 +0300

    Update LSTM.py

--- Uygulama/LSTM.py
+++ Uygulama/LSTM.py
@@ -4,13 +4,13 @@ from keras.preprocessing import sequence
 
 class Model:
     def Analiz(veri):
-        model = load_model('model.04-0.31.h5')
+        model = load_model('MODEL.h5')
         
-        num_words = 1000
+        num_words = 2000
         tok = Tokenizer(num_words=num_words)
         tok.fit_on_texts(veri)
         sequences = tok.texts_to_sequences(veri)
-        yorumlar_X = sequence.pad_sequences(sequences, padding = "pre", truncating ="pre",maxlen=293)
+        yorumlar_X = sequence.pad_sequences(sequences, padding = "pre", truncating ="pre",maxlen=279)
         
         predx = model.predict(yorumlar_X)
         y_pred = [1 if i > 0.5 else 0 for i in predx]
